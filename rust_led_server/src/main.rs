a06ab04300da2d3716755ab4bfab845e0b24a57c
dai Li <daiybh@gmail.com>
2023-05-29 21:02:59 +0800

    use  tinyhttp

--- rust_led_server/src/main.rs
+++ rust_led_server/src/main.rs
@@ -1,49 +1,9 @@
-
-
 use std::thread;
 mod tcp_server;
 use crate::tcp_server::std_tcp_server::StdTcpServer;
+use tiny_http::{Response, Server};
 
-#[macro_use] extern crate rocket;
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-use rocket::{Request, Data};
-use rocket::route::{Handler, Route, Outcome};
-use rocket::http::Method;
-#[derive(Copy, Clone)]
-enum Kind {
-    Simple,
-    Intermediate,
-    Complex,
-}
-
-#[derive(Clone)]
-struct CustomHandler(Kind);
-
-
-#[rocket::async_trait]
-impl Handler for CustomHandler {
-    async fn handle<'r>(&self, req: &'r Request<'_>, data: Data<'r>) -> Outcome<'r> {
-        match self.0 {
-            Kind::Simple => Outcome::from(req, "simple"),
-            Kind::Intermediate => Outcome::from(req, "intermediate"),
-            Kind::Complex => Outcome::from(req, "complex"),
-        }
-    }
-}
-
-impl Into<Vec<Route>> for CustomHandler {
-    fn into(self) -> Vec<Route> {
-        vec![Route::new(Method::Get, "/", self)]
-    }
-}
-
-
-#[rocket::main]
-async fn main() {
+fn main() {
     let mut a = StdTcpServer::New(8080, 10008);
     let mut a_clone = a.clone();
     thread::spawn(move || {
@@ -51,21 +11,32 @@ async fn main() {
         let mut input = String::new();
         loop {
             std::io::stdin().read_line(&mut input).unwrap();
-            println!("{}>>{}",input.len(), input);
-            if input.len() <3 {
-               
+            println!("{}>>{}", input.len(), input);
+            if input.len() < 3 {
                 a_clone.displayClients();
                 continue;
             }
             a_clone.sendMessage(input.as_bytes());
-            
+
             input.clear();
         }
     });
     //let rocket=  rocket::build().mount("/", routes![index]);
-    let rocket =  rocket::build().mount("/", CustomHandler(Kind::Simple))
-    .mount("/a", CustomHandler(Kind::Intermediate))
-    .mount("/b", CustomHandler(Kind::Complex));
-    rocket.launch().await;
-     println!("Hello, world!");
+
+    let server = Server::http("0.0.0.0:8000").unwrap();
+
+    for request in server.incoming_requests() {
+        println!(
+            "received request! method: {:?}, url: {:?}, headers: {:?}",
+            request.method(),
+            request.url(),
+            request.headers()
+        );
+        let response = Response::from_string("hello world");
+        request.respond(response);
+    }
+
+    println!("Hello, world!");
+    //maybe useful
+    //https://github.com/tomaka/rouille/blob/master/examples/database.rs  
 }
