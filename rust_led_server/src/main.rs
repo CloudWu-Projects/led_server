c74b4047d8ba94f001f89bfd1eb25bfa96f1f92b
dai Li <daiybh@gmail.com>
2023-05-29 20:06:06 +0800

    use std to do

--- rust_led_server/src/main.rs
+++ rust_led_server/src/main.rs
@@ -1,27 +1,27 @@
+
+
+use std::thread;
 mod tcp_server;
-use std::{thread, time};
-
-use tokio::join;
-
-use crate::tcp_server::TcpServer;
-#[tokio::main]
-async fn main() {
-    let local_port = 10008;
-    let remote_port = 10009;
-    let mut tcpS = TcpServer::new(local_port, remote_port);
-
-    let a = tcpS.start_tcp_server();
-
-    let loopthread = thread::spawn(move || {
-        let mut acount = 0;
+use crate::tcp_server::std_tcp_server::StdTcpServer;
+fn main(){
+    let mut a = StdTcpServer::New(8080, 10008);
+    let mut a_clone = a.clone();
+    thread::spawn(move || {
+        let mut input = String::new();
         loop {
-            thread::sleep(time::Duration::from_secs(1));
-            println!("{:?}", acount);
-
-            acount = acount + 1;
+            std::io::stdin().read_line(&mut input).unwrap();
+            println!("{}>>{}",input.len(), input);
+            if input.len() <3 {
+               
+                a_clone.displayClients();
+                continue;
+            }
+            a_clone.sendMessage(input.as_bytes());
+            
+            input.clear();
         }
     });
 
-    join!(a);
-    println!("over !!!!!!!!!!!")
+    a.start_server();
+
 }
--- rust_led_server/src/tcp_server/mod.rs
+++ rust_led_server/src/tcp_server/mod.rs
@@ -1,3 +1,7 @@
 mod tcp_server;
 
 pub use tcp_server::TcpServer;
+
+pub(crate) mod std_tcp_server;
+
+pub use std_tcp_server::StdTcpServer;
