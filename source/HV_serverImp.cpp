e9822c249eabee11fd5752e8f0dacc17e170f6ba
dai Li <daiybh@gmail.com>
2023-05-30 20:12:57 +0800

    don't output

--- source/HV_serverImp.cpp
+++ source/HV_serverImp.cpp
@@ -223,13 +223,13 @@ inline int HV_serverImp::tcp_server(int _proxy_port, int _backend_port) {
 			printf("%s connected! connfd=%d id=%d tid=%ld\n", peeraddr.c_str(), channel->fd(), channel->id(), currentThreadEventLoop->tid());
 
 			hio_setup_tcp_upstream(channel->io(), "127.0.0.1", backend_port, false);
-			hio_t * upstream = hio_get_upstream(channel->io());
+			/*hio_t * upstream = hio_get_upstream(channel->io());
 			SocketChannel sc(upstream);
 
 			printf("UP %s[%s] connected! connfd=%d id=%d tid=%ld\n", 
 			sc.peeraddr().c_str(), 
 			sc.localaddr().c_str(),
-			sc.fd(), sc.id(), currentThreadEventLoop->tid());
+			sc.fd(), sc.id(), currentThreadEventLoop->tid());/**/
 
 		}
 		else {
