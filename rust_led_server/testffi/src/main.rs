52dac9fec0ffc7126134035048291ceee9f1ef42
Dai Li <d.li@simplylive.tv>
2023-05-30 16:17:03 +0800

    add need func

--- rust_led_server/testffi/src/main.rs
+++ rust_led_server/testffi/src/main.rs
@@ -13,25 +13,30 @@ use libloading::{Library, Symbol};
 struct Object {
     _private: [u8; 0],
 }
-
+type LPAREARECT=*const u8;
+type HPROGRAM=*const u8;
 
 type    LedServerCallback=unsafe extern "C"  fn(Msg:c_int, wParam:c_int, lParam:*const u8)->c_int;
 type    LV_InitServerFn =unsafe extern "C" fn(port: c_int) -> c_int;
-
-
+type   LV_CreateProgramEx=unsafe extern "C" fn(LedWidth:c_int,LedHeight:c_int,ColorType:c_int,GrayLevel:c_int,SaveType:c_int)->HPROGRAM;
+type  LV_AddProgram=unsafe extern "C" fn(hProgram:HPROGRAM,ProgramNo:c_int, ProgramTime:c_int, LoopCount:c_int)->c_int;
+type LV_AddImageTextArea=unsafe extern "C" fn(hProgram:HPROGRAM,ProgramNo:c_int,AreaNo:c_int,pAreaRect:LPAREARECT,IsBackgroundArea:c_int)->c_int;
+type LV_DeleteProgram=unsafe extern "C" fn(hProgram:HPROGRAM );
 
 struct LedWrapper{
     lib: Library,
-    pub init_server_fn: RawSymbol<LV_InitServerFn> ,
+    init_server_fn: RawSymbol<LV_InitServerFn> ,
+    callback: RawSymbol<LedServerCallback>, 
+    create_program_ex: RawSymbol<LV_CreateProgramEx>,
+    add_program: RawSymbol<LV_AddProgram>,
+    add_image_text_area: RawSymbol<LV_AddImageTextArea>,
+    delete_program: RawSymbol<LV_DeleteProgram>,
+
 }
 
 impl LedWrapper{
     fn new() -> Self {
 
-    
-        
-    
-    
         let lib_path = match (std::env::consts::OS,std::env::consts::ARCH) {        
             ("windows", "x86_64") =>"lv_led_MBCS_64.dll",
             ("windows", "x86") =>"lv_led_MBCS_32.dll",
@@ -51,9 +56,34 @@ impl LedWrapper{
         let init_server_fnx: Symbol<LV_InitServerFn> = unsafe { lib.get(funcName_LV_LedInitServer.as_bytes()).unwrap() };
         let init_server_fnx =  unsafe { init_server_fnx.into_raw()};
 
+        let callback_fun: Symbol<LedServerCallback> = unsafe { lib.get(b"_Z25LV_RegisterServerCallbackPFiiiPvE").unwrap() };
+        let callback_fun=  unsafe { callback_fun.into_raw()};
+
+        let create_program_ex: Symbol<LV_CreateProgramEx> = unsafe { lib.get(b"_Z18LV_CreateProgramExiiiii\0").unwrap() };
+        let create_program_ex=  unsafe { create_program_ex.into_raw()};
+
+        let add_program: Symbol<LV_AddProgram> = unsafe { lib.get(b"_Z13LV_AddProgramPviii\0").unwrap() };
+        let add_program=  unsafe { add_program.into_raw()};
+
+        let add_image_text_area: Symbol<LV_AddImageTextArea> = unsafe { lib.get(b"_Z19LV_AddImageTextAreaPviiP8AREARECTb\0").unwrap() };
+        let add_image_text_area=  unsafe { add_image_text_area.into_raw()};
+
+        let delete_program: Symbol<LV_DeleteProgram> = unsafe { lib.get(b"_Z16LV_DeleteProgramPv\0").unwrap() };
+        let delete_program=  unsafe { delete_program.into_raw()};
+
         LedWrapper{
             lib: lib,
             init_server_fn: init_server_fnx,
+            callback: callback_fun,
+            create_program_ex: create_program_ex,
+            add_program: add_program,
+            add_image_text_area: add_image_text_area,
+            delete_program:delete_program,
+        }
+    }
+    pub fn init_server(&self, port: c_int) -> c_int {
+        unsafe {
+            (self.init_server_fn)(port)
         }
     }
 }
@@ -64,7 +94,7 @@ fn main() {
     
     // Call the function
     let port = 1234;
-    let result = unsafe { (wrapper.init_server_fn)(port) };
+    let result = wrapper.init_server(port);
     if result != 0 {
         panic!("Failed to initialize server: {}", result);
     }
